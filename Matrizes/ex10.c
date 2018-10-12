ae88138f2bc82ad39fa250c9701439121055a658
Philipe Aguiar Marques <34419519+xRiku@users.noreply.github.com>
2018-10-12 10:24:21 -0300

    Add files via upload

--- Matrizes/ex10.c
+++ Matrizes/ex10.c
@@ -1,7 +1,6 @@
 
-
-
 #include <stdio.h>
+#include <string.h>
 
 
 void estrela(int a){
@@ -9,9 +8,15 @@ void estrela(int a){
         putchar('*');
     putchar('\n');
 }
+int isequal(int n, int m){
+  if (n == m)
+    return 1;
+  else 
+    return 0;  
+}
 
 int main (){
-    int i1, j1, i2, j2, i3, j3, i, j; 
+    int i1, j1, i2, j2, i3, j3, i, j, k, x = 0;
     printf("Digite aqui as linhas e colunas da matriz A: ");
     scanf("%d %d", &i1, &j1);
     printf("Digite os numeros da matriz: ");
@@ -36,21 +41,42 @@ int main (){
     estrela(13);
     for (i = 0; i < i1; i++){
         for (j = 0; j < j1; j++)
-             printf("%d ", A[i][j]);
+             printf("%2d ", A[i][j]);
     putchar('\n');
     }
     estrela(13);
     for (i = 0; i < i2; i++){
         for (j = 0; j < j2; j++)
-             printf("%d ", B[i][j]);
+             printf("%2d ", B[i][j]);
     putchar('\n');
     }
     estrela(13);
     for (i = 0; i < i3; i++){
         for (j = 0; j < j3; j++)
-             printf("%d ", C[i][j]);
+             printf("%2d ", C[i][j]);
     putchar('\n');
     }
-             
-    
+    int D[i1][j2];
+    memset( D, 0, i1*j2*sizeof(int));
+    if (j1 == i2 && i1 == i3 && j3 == j2 ){
+        for (i = 0; i < i1; i++)
+            for(j = 0; j < j2; j++){
+                for (k = 0; k < j1; k++)
+                    D[i][j] = D[i][j] + A[i][k] * B[k][j];
+            if (isequal(D[i][j], C[i][j]))
+                x++;
+            }
+    }
+    else 
+        printf("DIFERENTE.\n");
+    estrela(13);
+    for (i = 0; i < i1; i++){
+        for (j = 0; j < j2; j++)
+             printf("%2d ", D[i][j]);
+    putchar('\n');
+    }
+    if (x == j3*i3)
+        printf("IGUAL.\n");
+    else    
+        printf("DIFERENTE.\n");
 }
